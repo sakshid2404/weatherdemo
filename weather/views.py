0215e0365fc18746bb4de7212f49f9d9252b0fdf
sakshi <sdesale2004@gmail.com>
2025-07-10 15:24:14 +0530

    a

---
+++ weather/views.py
@@ -0,0 +1,23 @@
+from cairo import Status
+from django.shortcuts import render,HttpResponse
+import requests
+
+def index(request):
+    req=requests.get(f"https://api.weatherapi.com/v1/current.json?q={request.GET.get("city")}&key=21520b1bb69a4d17b6125708250207").json()
+    print(req['current']['condition']['text'])
+   
+
+    return render(
+        request=request,
+        template_name="weather/index.html",
+        context={
+            'req':req,
+             'city':request.GET.get('city'),
+             
+        }
+    ) 
+    return HttpResponse(req['current']['condition']['text'])
+    
+
+
+
