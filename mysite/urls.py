99b668eeac724be9a40957e0f19be30126104fb2
sakshi <sdesale2004@gmail.com>
2026-02-20 14:15:04 +0530

    done

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-        path("weather/", include("weather.urls")),
+        path("", include("weather.urls")),
 
     path('admin/', admin.site.urls),
 ]
